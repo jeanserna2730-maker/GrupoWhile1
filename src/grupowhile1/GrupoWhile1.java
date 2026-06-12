f627594f187f598254e05fb1f4d1c0677865628a
Josue01 <josue.telenchana3175@utc.edu.ec>
2026-06-12 00:19:24 -0500

    CAJERO AUTOMATICO / Menu principal del cajero

--- src/grupowhile1/GrupoWhile1.java
+++ src/grupowhile1/GrupoWhile1.java
@@ -28,7 +28,44 @@ public class GrupoWhile1 {
                            """);
         while (intentos > 0 && intentos <=3) {
             int clave_ingresada = sc.nextInt();
-            
+            if (clave_ingresada == clave) {
+                do {
+                    System.out.println("""
+                                       -----------------------------------------------------
+                                                      BIENVENIDO AL CAJERO
+                                       -----------------------------------------------------
+                                       1. Consultar saldo
+                                       2. Retirar dinero
+                                       3. Depositar dinero
+                                       4. Salir
+                                       Ingrese la opcion deseada:
+                                       """);
+                    opccion = sc.nextInt();
+                    switch (opccion) {
+                        case 1:
+                            System.out.println("Consultar saldo:");
+                            break;
+                        case 2:
+                            System.out.println("Retirar dinero:");
+                            break;
+                        case 3:
+                            System.out.println("Depositar dinero:");
+                            break;
+                        case 4:
+                            System.out.println("Gracias por elegirnos!!!");
+                            break;
+                        default:
+                            intentos_opccion --;
+                            System.out.println("Te quedan "+intentos_opccion+" intentos!");
+                            System.out.println("INGRESE UNA OPCIÓN VALIDA!!!");
+                    }
+                } while (opccion != 4 && intentos_opccion >0); 
+                break;
+                
+            } else {
+                intentos = intentos -1;
+                System.out.println("Clave incorrecta. Te quedan " + intentos + " intentos");
+            }
         }
     }
 }
